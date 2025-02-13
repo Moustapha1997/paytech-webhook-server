c9f91643a53d847d60d021f82cb3731ac524127a
CapiataineJo <128262700+Moustapha1997@users.noreply.github.com>
2025-02-13 23:58:53 +0000

    Update paytech.ts

--- src/types/paytech.ts
+++ src/types/paytech.ts
@@ -1,15 +1,20 @@
+export interface CustomField {
+    ref_command: string;
+    redirect_after_success?: string;
+}
+
 export interface PayTechNotification {
-    type_event: 'sale_complete' | 'sale_canceled'
-    client_phone: string
-    payment_method: string
-    item_name: string
-    item_price: string
-    ref_command: string
-    command_name: string
-    currency: string
-    env: 'test' | 'prod'
-    custom_field: string
-    token: string
-    api_key_sha256: string
-    api_secret_sha256: string
+    type_event: 'sale_complete' | 'sale_canceled';
+    client_phone: string;
+    payment_method: string;
+    item_name: string;
+    item_price: string;
+    ref_command: string;
+    command_name: string;
+    currency: string;
+    env: 'test' | 'prod';
+    custom_field: string | CustomField;
+    token: string;
+    api_key_sha256: string;
+    api_secret_sha256: string;
 }
